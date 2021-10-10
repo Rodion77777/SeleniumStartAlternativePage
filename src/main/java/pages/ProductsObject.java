b18e8d7533be9e3aa43a552251846384ccd3e7b9
Rodion <rodionchik17@gmail.com>
2021-10-10 23:00:23 +0300

    added ProductObject

--- src/main/java/pages/ProductsObject.java
+++ src/main/java/pages/ProductsObject.java
@@ -1,4 +1,31 @@
-package pages;
+package alternate_pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import java.util.List;
 public class ProductsObject {
+
+    private EventFiringWebDriver eventDriver;
+
+    public ProductsObject (EventFiringWebDriver eventDriver) {
+        this.eventDriver = eventDriver;
+    }
+
+    public List<WebElement> itemFinder () {
+        return eventDriver.findElements(By.xpath("//*[@id=\"center_column\"]/ul/li"));
+    }
+
+    public List<WebElement> priceFinder2 (WebElement element) {
+        return element.findElements(By.xpath("div/div[2]/div[1]/span"));
+    }
+
+    public List<WebElement> popItemFinder () {
+        return eventDriver.findElements(By.xpath("//*[@id=\"homefeatured\"]/li"));
+    }
+
+    public WebElement nameFinder (WebElement element) {
+        return element.findElement(By.xpath("div/div[2]/h5/a"));
+    }
 }
